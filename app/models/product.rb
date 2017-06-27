50fb1476e3405f885620fa93533713ae9dc51f5a
marshall <l1pt0n1905@gmail.com>
2017-06-27 12:41:36 +0600

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,22 @@
 class Product < ApplicationRecord
-  belongs_to :category, dependent: :destroy
+  belongs_to :category
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_attached_file :image,
             styles: {thumbnail: "60x60", large: "300x300"}
   validates_attachment :image, presence: true, content_type: {content_type: ["image/png", "image/jpeg"]},  size: {in: 1..5000.kilobytes}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Cуществуют товарные позиции')
+      return false
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626105844) do
+ActiveRecord::Schema.define(version: 20170627060003) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +25,15 @@ ActiveRecord::Schema.define(version: 20170626105844) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -27,12 +41,12 @@ ActiveRecord::Schema.define(version: 20170626105844) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "price"
-    t.string   "category_id"
-    t.boolean  "available"
+    t.integer  "price"
+    t.integer  "category_id"
+    t.boolean  "available",          default: true
     t.integer  "discount"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "users", force: :cascade do |t|
